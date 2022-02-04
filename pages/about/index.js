0fa13010d2dbc4f7cc1983fd27b3dd178e875047
vuteyrin <97207559+MalyVutey@users.noreply.github.com>
2022-02-04 17:31:55 +0700

    Update index.js

--- pages/about/index.js
+++ pages/about/index.js
@@ -21,7 +21,7 @@ export async function getStaticProps() {
   }
 }
 const About = ({aboutdata}) => {
-  console.log(documentToReactComponents(aboutdata[1].fields.body));
+  console.log(documentToReactComponents(aboutdata[0].fields.body));
  return ( 
   <Box bg={"#FFFF"}>
   <Head>
